0127d86b6441bdf310e42a50da9d727fecd121ad
PierreOudin <pierre.oudin76@outlook.fr>
2024-04-21 14:40:20 +0200

    add dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 pub mod services;
 pub mod server_builder;
 
+use dotenv::dotenv;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+
     let addr = "[::1]:50051".parse()?;
     server_builder::build_server(addr).await?;
     Ok(())
